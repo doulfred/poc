4a235ffd1638d35d9ccef611b3e46db662539f80
doulfred@gmail.com <Toto@VOYAGEUR27.AGT-GROUPE.pri>
2012-12-14 12:01:56 +0100

    update structure

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
-app.get('/', function (req, res) {
-	console.log("Requête reçue pour le chemin." + __dirname);
-	res.sendfile(__dirname + '/public/index.html');
-});
-
-app.use("/public", express.static(__dirname + '/public'));
-
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+// Module de lancement : pour une conception evenementielle
+function start() {
+	app.get('/', function (req, res) {
+		console.log("Requête reçue pour le chemin." + __dirname);
+		res.sendfile(__dirname + '/public/index.html');
+	});
+
+	app.use("/public", express.static(__dirname + '/public'));
+
+
+	app.listen(port, function() {
+	  console.log("Listening on " + port);
+	});
+}
+
+
+start();
