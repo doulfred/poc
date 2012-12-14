4a06387319682ef7354525e149ca1251453e258e
doulfred@gmail.com <Toto@VOYAGEUR27.AGT-GROUPE.pri>
2012-12-14 12:18:35 +0100

    update structure

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var server = require("./server");
-/*var router = require("./router");
+var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
 // gestionnaires de requete => liste d'action
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
-//handle["/styles/style.css"] = requestHandlers.style;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
-*/
+
 // on mance le serveur en tranmetant le routeur + les "actions"
-server.start(/*router.route, handle*/);
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var app = express.createServer(express.logger());
+var url = require("url");
 var port = process.env.PORT || 5000;
 
 // Module de lancement : pour une conception evenementielle
-function start() {
+function start(route, handle) {
+	/*
 	app.get('/', function (req, res) {
 		console.log("Requête reçue pour le chemin." + __dirname);
 		res.sendfile(__dirname + '/public/index.html');
 	});
-
+	*/
 	app.use("/public", express.static(__dirname + '/public'));
 
 
+	
+	for(var attributename in handle){
+		console.log(attributename/*+": "+handle[attributename]*/);
+		app.get(attributename, function (request, response) {
+			console.log("Requête reçue pour le chemin "+attributename+".");
+			//handle[attributename](response, request);
+			var pathname = url.parse(request.url).pathname;
+			route(handle, pathname, response, request);
+		});
+	}
+	
+	app.post('/upload', function (req, res) {
+		console.log("Requête reçue pour le chemin.");
+		handle["/upload"](res, req);
+	});
+	
+	
+	
+	
+	
 	app.listen(port, function() {
 	  console.log("Listening on " + port);
 	});
